ce7166a5a27860d2fc32e0f83ec357ea25af2fed
ZOKOISC <7878836+ZOKOISC@users.noreply.github.com>
2026-09-16 21:19:56 +0200

    new 9

--- src/functions/custom/CellDialog.java
+++ src/functions/custom/CellDialog.java
@@ -24,6 +24,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
